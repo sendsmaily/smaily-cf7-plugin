b601d3079090ea7954e1a6c7fdf343d08f337a31
TomAbel <tomkristian.abel@tptlive.ee>
2020-03-03 17:04:12 +0200

    Add core plugin class & loader file

---
+++ includes/class-smaily-for-cf7.php
@@ -0,0 +1,195 @@
+<?php
+/**
+ * The file that defines the core plugin class
+ *
+ * A class definition that includes attributes and functions used across both the
+ * public-facing side of the site and the admin area.
+ *
+ * @link       https://github.com/sendsmaily/smaily-cf7-plugin
+ * @since      1.0.0
+ *
+ * @package    Smaily_For_CF7
+ * @subpackage Smaily_For_CF7/includes
+ */
+
+/**
+ * The core plugin class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      1.0.0
+ * @package    Smaily_For_CF7
+ * @subpackage Smaily_For_CF7/includes
+ */
+class Smaily_For_CF7 {
+
+	/**
+	 * The loader that's responsible for maintaining and registering all hooks that power
+	 * the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      Smaily_For_CF7_Loader    $loader    Maintains and registers all hooks for the plugin.
+	 */
+	protected $loader;
+
+	/**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $smaily_for_cf7    The string used to uniquely identify this plugin.
+	 */
+	protected $smaily_for_cf7;
+
+	/**
+	 * The current version of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $version    The current version of the plugin.
+	 */
+	protected $version;
+
+	/**
+	 * Define the core functionality of the plugin.
+	 *
+	 * Set the plugin name and the plugin version that can be used throughout the plugin.
+	 * Load the dependencies, and set the hooks for the admin area and
+	 * the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct() {
+		if ( defined( 'SMAILY_FOR_CF7_VERSION' ) ) {
+			$this->version = SMAILY_FOR_CF7_VERSION;
+		} else {
+			$this->version = '1.0.0';
+		}
+		$this->smaily_for_cf7 = 'smaily_for_cf7';
+
+		$this->load_dependencies();
+		$this->define_admin_hooks();
+		$this->define_public_hooks();
+	}
+
+	/**
+	 * Load the required dependencies for this plugin.
+	 *
+	 * Include the following files that make up the plugin:
+	 *
+	 * - Smaily_For_CF7_Loader. Orchestrates the hooks of the plugin.
+	 * - Smaily_For_CF7_Admin. Defines all hooks for the admin area.
+	 * - Smaily_For_CF7_Public. Defines all hooks for the public side of the site.
+	 *
+	 * Create an instance of the loader which will be used to register the hooks
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function load_dependencies() {
+
+		/**
+		 * The class responsible for orchestrating the actions and filters of the
+		 * core plugin.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-smaily-for-cf7-loader.php';
+
+		/**
+		 * The class responsible for defining all actions that occur in the admin area.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-smaily-for-cf7-admin.php';
+
+		/**
+		 * The class responsible for defining all actions that occur in the public-facing
+		 * side of the site.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-smaily-for-cf7-public.php';
+
+		$this->loader = new Smaily_For_CF7_Loader();
+	}
+
+	/**
+	 * Register all of the hooks related to the admin area functionality
+	 * of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_admin_hooks() {
+
+		$plugin_admin = new Smaily_For_CF7_Admin( $this->get_smaily_for_cf7(), $this->get_version() );
+
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+
+		$this->loader->add_action( 'wpcf7_after_save', $plugin_admin, 'save' );
+		$this->loader->add_action( 'wpcf7_editor_panels', $plugin_admin, 'add_tab', -1 );
+		$this->loader->add_action( 'wpcf7_default_template', $plugin_admin, 'replace_template', 10, 2 );
+		$this->loader->add_action( 'wpcf7_skip_mail', $plugin_admin, 'disable_mail', 10, 2 );
+
+		$this->loader->add_action( 'wp_ajax_verify_credentials_callback', $plugin_admin, 'verify_credentials_callback' );
+		$this->loader->add_action( 'wp_ajax_nopriv_verify_credentials_callback', $plugin_admin, 'verify_credentials_callback' );
+
+		$this->loader->add_action( 'wp_ajax_remove_credentials_callback', $plugin_admin, 'remove_credentials_callback' );
+		$this->loader->add_action( 'wp_ajax_nopriv_remove_credentials_callback', $plugin_admin, 'remove_credentials_callback' );
+	}
+
+	/**
+	 * Register all of the hooks related to the public-facing functionality
+	 * of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_public_hooks() {
+
+		$plugin_public = new Smaily_For_CF7_Public( $this->get_smaily_for_cf7(), $this->get_version() );
+		$this->loader->add_action( 'wpcf7_submit', $plugin_public, 'submit', 10, 2 );
+
+	}
+
+	/**
+	 * Run the loader to execute all of the hooks with WordPress.
+	 *
+	 * @since    1.0.0
+	 */
+	public function run() {
+		$this->loader->run();
+	}
+
+	/**
+	 * The name of the plugin used to uniquely identify it within the context of
+	 * WordPress and to define internationalization functionality.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The name of the plugin.
+	 */
+	public function get_smaily_for_cf7() {
+		return $this->smaily_for_cf7;
+	}
+
+	/**
+	 * The reference to the class that orchestrates the hooks with the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    Smaily_For_CF7_Loader    Orchestrates the hooks of the plugin.
+	 */
+	public function get_loader() {
+		return $this->loader;
+	}
+
+	/**
+	 * Retrieve the version number of the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The version number of the plugin.
+	 */
+	public function get_version() {
+		return $this->version;
+	}
+}
