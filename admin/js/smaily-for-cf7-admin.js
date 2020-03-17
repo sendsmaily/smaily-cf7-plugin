7d39fefe3fed6173c853b6025054c850b5fe3372
TomAbel <tomkristian.abel@tptlive.ee>
2020-03-17 14:39:56 +0200

    Invalid identation for jQuery function

--- admin/js/smaily-for-cf7-admin.js
+++ admin/js/smaily-for-cf7-admin.js
@@ -1,13 +1,12 @@
 (function( $ ) {
 	function getUrlParam(name) {
 		var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-			if (results == null) {
-				return null;
-			}
-			else {
-				return decodeURI(results[1]) || 0;
-			}
-
+		if (results == null) {
+			return null;
+		}
+		else {
+			return decodeURI(results[1]) || 0;
+		}
 	}
 	$(document).ready(function () {
 		$('#smailyforcf7_validate_credentials').click(function () {
