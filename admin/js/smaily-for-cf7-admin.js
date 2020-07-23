3ee5b18d8698e9c5266113c6f2fa29463dc9ec8d
TomAbel <tomkristianabel@gmail.com>
2020-07-23 22:23:37 +0300

    Disable JS unbeforeload event
    This prompted a pop-up if you click on any link while
    in the CF7 form editor

--- admin/js/smaily-for-cf7-admin.js
+++ admin/js/smaily-for-cf7-admin.js
@@ -9,6 +9,9 @@
 		}
 	}
 	$(document).ready(function () {
+		// Disable onbeforeunload event.
+		jQuery(window).off("beforeunload", null);
+
 		$('#smailyforcf7_validate_credentials').click(function () {
 			$.post(smaily_for_cf7.ajax_url,
 			{
